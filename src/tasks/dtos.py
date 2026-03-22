b3abb818b4fa9f56901d1a7e1fc4b48703684be2
Akash chaudhary <akashchaudhary1326@gmail.com>
2026-03-23 00:02:54 +0530

    Refactor task creation and retrieval responses; enhance router with response models and status codes

--- src/tasks/dtos.py
+++ src/tasks/dtos.py
@@ -1,6 +1,26 @@
+from asyncio import Task
+
 from pydantic import BaseModel
 
 class TaskCreate(BaseModel):
     title: str
     description: str
     is_completed: bool = False
+
+
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    description: str
+    is_completed: bool
+
+
+class AllTasksResponse(BaseModel):
+    status: str
+    data: list[TaskResponse]
+
+
+class OneTaskResponse(BaseModel):
+    status: str
+    data: TaskResponse
--- src/tasks/router.py
+++ src/tasks/router.py
@@ -1,33 +1,35 @@
-from fastapi import  APIRouter , Depends
+from fastapi import  APIRouter , Depends , status
 from src.tasks import controller
-from src.tasks.dtos import TaskCreate
+from src.tasks.dtos import TaskCreate , TaskResponse , AllTasksResponse , OneTaskResponse
 from src.utils.db import get_db
+from sqlalchemy.orm import Session
+from typing import List
 task_routes = APIRouter(prefix='/tasks')
 
  
-@task_routes.post('/create_task')
-def create_task(body: TaskCreate , db = Depends(get_db)):
+@task_routes.post('/create_task', response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
+def create_task(body: TaskCreate , db: Session = Depends(get_db)):
     return controller.create_task(body ,db)
 
-@task_routes.get('/get_all_tasks')
-def get_tasks(db = Depends(get_db)):
+@task_routes.get('/get_all_tasks', response_model=AllTasksResponse, status_code=status.HTTP_200_OK)
+def get_tasks(db: Session = Depends(get_db)):
     return controller.get_tasks(db)
 
-@task_routes.get('/get_task_by_id/{id}')
-def get_task_by_id(id: int , db = Depends(get_db)):
+@task_routes.get('/get_task_by_id/{id}', response_model=OneTaskResponse, status_code=status.HTTP_200_OK)
+def get_task_by_id(id: int , db: Session = Depends(get_db)):
     return controller.get_task_by_id(id , db)
 
-@task_routes.put('/update_task/{id}')
-def update_task(id:int , body: TaskCreate , db = Depends(get_db)):
+@task_routes.put('/update_task/{id}', response_model=OneTaskResponse, status_code=status.HTTP_200_OK)
+def update_task(id:int , body: TaskCreate , db: Session = Depends(get_db)):
     return controller.update_task(id , body , db)
 
-@task_routes.delete('/delete_task/{id}')
-def delete_task(id:int , db = Depends(get_db)):
+@task_routes.delete('/delete_task/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(id:int , db: Session = Depends(get_db)):
     return controller.delete_task(id , db)
 
 
-@task_routes.put('/mark_task_as_completed/{id}')
-def mark_task_as_completed(id:int , db = Depends(get_db)):
+@task_routes.put('/mark_task_as_completed/{id}', response_model=OneTaskResponse, status_code=status.HTTP_200_OK)
+def mark_task_as_completed(id:int , db: Session = Depends(get_db)):
     return controller.mark_task_as_completed(id , db)
  
     
